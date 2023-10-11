8ce215fa41d7008c1d089e5078c00e89cac3ff9c
bapuji <babuji.cybrain@gmail.com>
2023-10-11 17:55:33 +0530

    first commit

---
+++ mvcEntity/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvcEntity.Data;
+using mvcEntity.Models;
+
+namespace mvcEntity.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AplicationDbContext _db;
+        public CategoryController(AplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> ObjcCategoriesList = _db.Categories.ToList();
+            return View(ObjcCategoriesList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Category name and display order can not be same.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ mvcEntity/Data/AplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvcEntity.Models;
+
+namespace mvcEntity.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base (options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData
+                (
+                    new Category { CategoryId = 1, Name = "Action", DisplayOrder = 1},
+                    new Category { CategoryId = 2, Name = "SiFi", DisplayOrder = 2 },
+                    new Category { CategoryId = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
